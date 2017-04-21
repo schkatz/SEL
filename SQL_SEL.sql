ddb591e6d9f30ada10f77b2ea0a518ad61f09c64
schkatz <maja.krusiewicz@gmail.com>
2017-04-21 14:30:45 +0200

    Update SQL_SEL.sql

--- SQL_SEL.sql
+++ SQL_SEL.sql
@@ -33,7 +33,163 @@ CREATE TABLE [Games] (
 
 
 
+SPRAWDŹ
 
+ 
+  -- -----------------------------------------------------
+-- Table [SEL].[Teams]
+-- -----------------------------------------------------
+CREATE TABLE [Teams] (
+  [Team_ID] INT NOT NULL,
+  [TeamLeague_ID] INT NOT NULL,
+  [TeamCaptain] INT NOT NULL,
+  [TeamName] VARCHAR(45) NULL,
+  [TeamGame] VARCHAR(45) NULL,
+  PRIMARY KEY ([Team_ID]),
+  INDEX [id_t_UNIQUE] ([Team_ID] ASC),
+  INDEX [fk_Teams_Users1_idx] ([TeamCaptain] ASC),
+  INDEX [fk_Teams_Leagues1_idx] ([TeamLeague_ID] ASC),
+  CONSTRAINT [fk_Teams_Users1]
+    FOREIGN KEY ([TeamCaptain])
+    REFERENCES [SEL].[Users] ([User_ID])
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT [fk_Teams_Leagues1]
+    FOREIGN KEY ([TeamLeague_ID])
+    REFERENCES [SEL].[Leagues] ([League_ID])
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+
+
+  -- -----------------------------------------------------
+-- Table [SEL].[Users]
+-- -----------------------------------------------------
+CREATE TABLE [Users] (
+  [User_ID] INT NOT NULL,
+  [UserTeam_ID] INT NULL,
+  [UserRole_ID] INT NOT NULL,
+  [UserFirstName] VARCHAR(45) NULL,
+  [UserLastName] VARCHAR(45) NULL,
+  [UserNick] VARCHAR(45) NOT NULL,
+  [UserAge] INT NULL,
+  [UserSchool] VARCHAR(45) NULL,
+  [UserPassword] VARCHAR(20) NOT NULL,
+  [UserEmailAdress] VARCHAR(45) NOT NULL,
+  PRIMARY KEY ([User_ID]),
+  INDEX [id_u_UNIQUE] ([User_ID] ASC),
+  INDEX [fk_Users_Teams_idx] ([UserTeam_ID] ASC),
+  INDEX [fk_Users_Roles1_idx] ([UserRole_ID] ASC),
+  INDEX [UserNick_UNIQUE] ([UserNick] ASC),
+  INDEX [UserEmailAdress_UNIQUE] ([UserEmailAdress] ASC),
+  CONSTRAINT [fk_Users_Teams]
+    FOREIGN KEY ([UserTeam_ID])
+    REFERENCES [SEL].[Teams] ([Team_ID])
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT [fk_Users_Roles1]
+    FOREIGN KEY ([UserRole_ID])
+    REFERENCES [SEL].[Roles] ([Role_ID])
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+
+
+
+-- -----------------------------------------------------
+-- Table [SEL].[Matchups]
+-- -----------------------------------------------------
+CREATE TABLE [Matchups] (
+  [Matchup_ID] INT NOT NULL,
+  [MatchupTeam_ID1] INT NOT NULL,
+  [MatchupTeam_ID2] INT NOT NULL,
+  [MatchupDate] DATE NOT NULL,
+  [MatchupTime] DATETIME NOT NULL,
+  PRIMARY KEY ([Matchup_ID]),
+  INDEX [fk_Matchups_Teams1_idx] ([MatchupTeam_ID1] ASC),
+  INDEX [fk_Matchups_Teams2_idx] ([MatchupTeam_ID2] ASC),
+  INDEX [Matchup_ID_UNIQUE] ([Matchup_ID] ASC),
+  CONSTRAINT [fk_Matchups_Teams1]
+    FOREIGN KEY ([MatchupTeam_ID1])
+    REFERENCES [SEL].[Teams] ([Team_ID])
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT [fk_Matchups_Teams2]
+    FOREIGN KEY ([MatchupTeam_ID2])
+    REFERENCES [SEL].[Teams] ([Team_ID])
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+
+
+-- -----------------------------------------------------
+-- Table [SEL].[Accounts]
+-- -----------------------------------------------------
+CREATE TABLE [Accounts] (
+  [AccountUser_ID] INT NOT NULL,
+  [AccountGame_ID] INT NOT NULL,
+  PRIMARY KEY ([AccountUser_ID], [AccountGame_ID]),
+  INDEX [fk_Users_has_Games_Games1_idx] ([AccountGame_ID] ASC),
+  INDEX [fk_Users_has_Games_Users1_idx] ([AccountUser_ID] ASC),
+  CONSTRAINT [fk_Users_has_Games_Users1]
+    FOREIGN KEY ([AccountUser_ID])
+    REFERENCES [SEL].[Users] ([User_ID])
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT [fk_Users_has_Games_Games1]
+    FOREIGN KEY ([AccountGame_ID])
+    REFERENCES [SEL].[Games] ([Game_ID])
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+
+
+
+-- -----------------------------------------------------
+-- Table [SEL].[PlayerStats]
+-- -----------------------------------------------------
+CREATE TABLE [PlayerStats] (
+  [PlayerStat_ID] INT NOT NULL,
+  [PlayerStatKills] INT NULL,
+  [PlayerStatAssists] INT NULL,
+  [PlayerStatDeaths] INT NULL,
+  [PlayerStatKDA] FLOAT NULL,
+  [PlayerStatWins] INT NULL,
+  [PlayerStatLoss] INT NULL,
+  [PlayerStatWinratio] FLOAT NULL,
+  [PlayerStatAccountUser_ID] INT NOT NULL,
+  [PlayerStatAccountGame_ID] INT NOT NULL,
+  PRIMARY KEY ([PlayerStat_ID]),
+  INDEX [fk_PlayerStats_Accounts1_idx] ([PlayerStatAccountUser_ID] ASC, [PlayerStatAccountGame_ID] ASC),
+  INDEX [PlayerStat_ID_UNIQUE] ([PlayerStat_ID] ASC),
+  CONSTRAINT [fk_PlayerStats_Accounts1]
+    FOREIGN KEY ([PlayerStatAccountUser_ID] , [PlayerStatAccountGame_ID])
+    REFERENCES [SEL].[Accounts] ([AccountUser_ID] , [AccountGame_ID])
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+
+
+-- -----------------------------------------------------
+-- Table [SEL].[TeamStats]
+-- -----------------------------------------------------
+CREATE TABLE [TeamStats] (
+  [TeamStats_ID] INT NOT NULL,
+  [TeamStatsWins] INT NULL,
+  [TeamStatsLoss] INT NULL,
+  [TeamStatsWinratio] FLOAT NULL,
+  [TeamPoints] INT NULL,
+  [TeamStatsTeam_ID] INT NOT NULL,
+  PRIMARY KEY ([TeamStats_ID]),
+  INDEX [TeamStats_ID_UNIQUE] ([TeamStats_ID] ASC),
+  INDEX [fk_TeamStats_Teams1_idx] ([TeamStatsTeam_ID] ASC),
+  CONSTRAINT [fk_TeamStats_Teams1]
+    FOREIGN KEY ([TeamStatsTeam_ID])
+    REFERENCES [SEL].[Teams] ([Team_ID])
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+  
+  
+  
+  KONIEC 
+  
+  
+  
 
 
 
